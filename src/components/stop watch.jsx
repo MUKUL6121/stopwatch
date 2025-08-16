82b23662709cc973e6529e6fade52ff3c80159d1
MUKUL6121 <148455498+MUKUL6121@users.noreply.github.com>
2025-08-16 12:12:58 +0530

    Add files via upload

--- src/components/stop watch.jsx
+++ src/components/stop watch.jsx
@@ -21,13 +21,13 @@ export default function stopwatch() {
         setState(0);
     }
     return (
-        <div style={{ border: "2px solid black", padding: "0px 20px 20px 20px" }}>
+        <div style={{width:"140%", border: "2px solid black", "border-radius": "10px", "background-color": "#000911", padding: "0px 20px 20px 20px" }}>
             <div><h1>Stopwatch</h1></div><hr style={{ color: "white", width: "100%" }} />
             <div>
-                <p style={{ border: "2px solid black", height: "20px", width: "20px", "border-radius": "50%", "background-color": color }}></p>
-                <h2><p>Time : </p><p>{state} Second</p></h2>
+                <p style={{ border: "2px solid black", height: "20px", width: "80px", "border-radius": "20%", "background-color": color }}></p>
+                <div><h2><p>Time : </p><p>{state} Sec</p></h2></div>
                 <button onClick={start} style={{ "background-color": "green" }}>Start</button>
-                <button onClick={stop} style={{ "background-color": "yellow" }}>Stop</button>
+                <button onClick={stop} style={{ "background-color": "#d8ce0aff" }}>Stop</button>
                 <button onClick={reset} style={{ "background-color": "red" }}>Reset</button>
             </div>
         </div>
